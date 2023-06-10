bf30751a7e3557eae635d00fa392c3014faaaa7d
Janet Villarreal <jvillarr20@gmail.com>
2023-06-09 17:12:55 -0700

    finished styling cards for responsiveness

--- src/app/components/Cards.js
+++ src/app/components/Cards.js
@@ -2,8 +2,10 @@ import styles from "../page.module.scss";
 export default function Card({ title, content, color }) {
   return (
     <div className={`${styles.card} ${color}`}>
-      <h2>{title}</h2>
-      <div className="separator-sm"></div>
+      <h2>
+        {title}
+        <div className="separator-sm"></div>
+      </h2>
       <div className={styles.innerContent}>
         <p>{content}</p>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -6,39 +6,45 @@ import Card from "./components/Cards";
 import data from "./data/data.json";
 export default function Home() {
   return (
-    <main className={styles.wrapper}>
-      <section className={styles.intro}>
-        <div className={styles.intro__modern}>
-          <p>Modern & Fun</p>
-        </div>
-        <h1>Online Learning Education</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-        <button className={styles.darkBtn}>Read More</button>
-        <div className={styles.hpImg}>
-          <Image
-            src={hpLearning}
-            alt="Heaphones Audio Learning"
-            priority="true"
-          />
-        </div>
-      </section>
-      <section className={styles.cards}>
-        {data.map((card) => {
-          return (
-            <Card
-              title={card.title}
-              content={card.content}
-              color={card.color}
-            />
-          );
-        })}
-      </section>
+    <>
+      <main className={styles.wrapper}>
+        <section className={styles.intro}>
+          <div className={styles.intro__wrapper}>
+            <div className={styles.intro__education}>
+              <div className={styles.intro__modern}>
+                <p>Modern & Fun</p>
+              </div>
+              <h1>Online Learning Education</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <button className={styles.darkBtn}>Read More</button>
+            </div>
+            <div className={styles.hpImg}>
+              <Image
+                src={hpLearning}
+                alt="Heaphones Audio Learning"
+                priority="true"
+              />
+            </div>
+          </div>
+        </section>
+        <section className={styles.cards}>
+          {data.map((card) => {
+            return (
+              <Card
+                title={card.title}
+                content={card.content}
+                color={card.color}
+              />
+            );
+          })}
+        </section>
+      </main>
       <footer className="footer">
         <Footer />
       </footer>
-    </main>
+    </>
   );
 }
