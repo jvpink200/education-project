42ebf5d4498a2f8a5b58dbf38f710a612e1d946b
Janet Villarreal <jvillarr20@gmail.com>
2023-07-18 15:28:37 -0700

    added an about page and updated contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,3 +1,16 @@
 export default function Contact() {
-  return <p>Contact Test</p>;
+  return (
+    <section>
+      <div className="circle-gray"></div>
+      <div>
+        <h1>What We Do</h1>
+        <h2>
+          Sample text. Click to select the text box. Click again or double click
+          to start editing the text.
+        </h2>
+        <button>contact us</button>
+      </div>
+      <div className="circle-sm-blue"></div>
+    </section>
+  );
 }
